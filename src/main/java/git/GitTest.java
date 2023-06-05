e2088686f360f9f5720d7adbf41844a67df40aed
melody <1520310854@qq.com>
2023-06-05 21:48:55 +0800

    1.新增：git-test

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * @author jack_chen
+ * @version 1.0
+ * @date 2023/6/5
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
